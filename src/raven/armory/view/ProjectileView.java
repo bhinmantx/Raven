6819e730aad3ded59f2f65ffb614a197e7e8fd78
Chester Husk III <chusk3@gmail.com>
2011-06-15 16:12:10 -0500

    Cleanups.

---
+++ src/raven/armory/view/ProjectileView.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package raven.armory.view;
+
+import raven.game.interfaces.IDrawable;
+
+/**
+ * @author chester
+ *
+ */
+public class ProjectileView implements IDrawable {
+
+	/**
+	 * 
+	 */
+	public ProjectileView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see raven.game.interfaces.IDrawable#render()
+	 */
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
